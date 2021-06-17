25e04afd10c2b498aedc4f0f7907789b3a51336c
b45i <amalshajan2011@gmail.com>
2021-06-17 18:24:45 +0530

    dynamic content

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const generateImage = require('./image-maker');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.get(`/`, async function (req, res) {
-    const image = await generateImage(
-        'Windows 11 May Be Available as a Free Upgrade for Windows 7, Windows 8.1 Users - Gadgets 360',
-        'https://i.gadgets360cdn.com/large/windows_11_image_microsoft_1623849610001.jpg',
-        '16-06-2021',
-        'Amal Shajan'
-    );
+    const title = req.query.title;
+    const imgURL = req.query.imgURL;
+    const date = req.query.date;
+    const author = req.query.author;
+
+    const image = await generateImage(title, imgURL, date, author);
     res.writeHead(200, { 'Content-Type': 'image/jpeg' });
     res.end(image, 'binary');
 });
